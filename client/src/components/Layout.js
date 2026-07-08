da8218f5c02e45ce9a057dba1fbb831be3482c39
Suryaaanshh <suryanshkashyap7@gmail.com>
2026-07-09 03:18:03 +0530

    Layout.js

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -11,8 +11,8 @@ function Layout({ children }) {
         <div className="brand">
           <img src={logo} alt="DevOps Shack logo" className="logo" />
           <div>
-            <h1 className="brand-title">DevOps Shack</h1>
-            <p className="nav-subtitle">User Management</p>
+            <h1 className="brand-title">PBL</h1>
+            <p className="nav-subtitle">Project Based Learning</p>
           </div>
         </div>
       </header>
@@ -21,9 +21,9 @@ function Layout({ children }) {
         <aside className="sidebar slide-in-left">
           <h3>Connect</h3>
           <ul className="social-links">
-            <li><a className="sidebar-btn" href="https://www.linkedin.com/in/adityajaiswal7/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-            <li><a className="sidebar-btn" href="https://www.youtube.com/@devopsshack" target="_blank" rel="noopener noreferrer">YouTube</a></li>
-            <li><a className="sidebar-btn" href="https://www.instagram.com/devopsshack" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+            <li><a className="sidebar-btn" href="https://www.linkedin.com/in/suryansh-kashyap-878706387//" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+            <li><a className="sidebar-btn" href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">YouTube</a></li>
+            <li><a className="sidebar-btn" href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
           </ul>
         </aside>
         <main className="main-content fade-in">
